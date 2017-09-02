02135c51ccb35620d0bdbb1598e6032b0806d39d
Dragon-Coder <chris.smith.web.master@gmail.com>
2017-09-02 13:49:48 -0700

    Next Level of Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Write("Press any key to continue...");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
             Console.ReadKey(true);
         }
     }
